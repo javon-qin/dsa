90443467d926fbd6521e953e06204e56d91b93bd
Javon Qin <563867631@qq.com>
2021-02-14 00:50:51 +0800

    Init project.

---
+++ src/main/java/cn/javon/dsa/a/leetcode/array/Tester.java
@@ -0,0 +1,12 @@
+package cn.javon.dsa.a.leetcode.array;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+
+}
